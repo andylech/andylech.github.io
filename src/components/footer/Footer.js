868afd59e1193f9887d4676cec3eb247a3d7496e
Andy Lech <github@andylech.com>
2024-07-02 21:20:35 -0400

    Change Footer to avoid confusion on ownership of DeveloperFolio theme

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,19 @@
 import React, {useContext} from "react";
 import "./Footer.scss";
-import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
 
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
-          </a>
-        </p>
-      </div>
+    <div className="footer-div">
+      <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+        Theme by{" "}
+        <a href="https://github.com/saadpasta/developerFolio">
+          developerFolio
+        </a>{" "}
+        team
+      </p>
+    </div>
   );
 }
