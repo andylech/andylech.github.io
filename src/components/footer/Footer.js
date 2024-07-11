9a9ae1599caae0b3cf8986aeeaf25a5455c6d3d7
Andy Lech <github@andylech.com>
2024-07-11 11:08:34 -0400

    Add link to this repo in Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,6 +7,10 @@ export default function Footer() {
   return (
     <div className="footer-div">
       <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+        Hosted on GitHub Pages from{" "}
+        <a href="https://github.com/andylech/andylech.github.io">andylech.github.io</a>
+      </p>
+      <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
         Original theme by{" "}
         <a href="https://github.com/saadpasta/developerFolio">developerFolio</a>{" "}
         team
