9afff73e4c9bf03f31099e51063c0ccf6551e433
Andy Lech <github@andylech.com>
2024-07-26 10:54:17 -0400

    Clean up old attempts to make Sessionize icon like Font Awesome glyphs

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -128,18 +128,6 @@ export default function socialMedia() {
         </a>
       ) : null}
 
-      {/* {socialMediaLinks.sessionize ? (
-        <a
-          href={socialMediaLinks.sessionize}
-          className="icon-button sessionize"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={"./sessionizeLogo.png"} alt="Sessionize"/>
-          <span></span>
-        </a>
-      ) : null} */}
-
       {socialMediaLinks.sessionize ? (
         <a
           href={socialMediaLinks.sessionize}
@@ -155,20 +143,6 @@ export default function socialMedia() {
         </a>
       ) : null}
 
-      {/* {socialMediaLinks.sessionize ? (
-        <a
-          href={socialMediaLinks.sessionize}
-          className="icon-button sessionize"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={require("./assets/images/sessionizeLogo.png")}
-            alt="Sessionize"
-          />
-          <span></span>
-        </a>
-      ) : null} */}
     </div>
   );
 }
